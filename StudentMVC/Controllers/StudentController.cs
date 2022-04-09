3362d75524751da8d3f4352ba42a53f197a5a0f9
Diksha-Lamichhane <lamichhanediksha17@gmail.com>
2022-04-09 16:45:09 +0545

    first commit

---
+++ StudentMVC/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using StudentMVC.Models;
+using StudentMVC.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StudentServices _stuServices;
+        // GET: Student
+        public ActionResult List()
+        {
+            _stuServices = new StudentServices();
+
+            var model = _stuServices.GetStudentList();
+
+            return View(model);
+        }
+
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddStudent(StudentModel model)
+        {
+            _stuServices = new StudentServices();
+
+            _stuServices.InsertStudent(model);
+
+            return RedirectToAction("List");
+        }
+
+        public ActionResult EditStudent(int Student_ID = 0)
+        {
+            _stuServices = new StudentServices();
+
+            var model = _stuServices.GetEditById(Student_ID);
+
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult EditStudent(StudentModel model)
+        {
+            _stuServices = new StudentServices();
+
+            _stuServices.UpdateStudent(model);
+
+            return RedirectToAction("List");
+        }
+
+        //[HttpPost]
+        public ActionResult DeleteStudent(int Student_ID)
+        {
+            _stuServices = new StudentServices();
+
+            _stuServices.DeleteStudent(Student_ID);
+
+            return RedirectToAction("List");
+        }
+
+    }
+}
---
+++ StudentMVC/Models/StudentModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentMVC.Models
+{
+    public class StudentModel
+    {
+        public int Student_ID { get; set; }
+
+        [Required(ErrorMessage = "Name is required..!!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required..!!")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Phone is required..!!")]
+        public string Phone { get; set; }
+
+    }
+}
